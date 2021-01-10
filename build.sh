fec967f34fd4dd82320453a8dbbf340390d6499d
benkyriakou <benkyriakou@users.noreply.github.com>
2021-01-10 12:32:34 +0000

    Added template inclusion functionality, buymeacoffee and Google Analytics templates.

--- build.sh
+++ build.sh
@@ -7,3 +7,12 @@ asciidoctor \
 	--out-file index.html \
 	--trace \
 	drupal-widget-guide.adoc
+
+HEAD="$(sed '/<\/head>/q' index.html | head -n -1)"
+HEADCOUNT=$(( "$(echo "$HEAD" | wc -l)" + 1))
+FOOT="$(tail -n +${HEADCOUNT} index.html)"
+
+echo "$HEAD" > index.html
+cat "templates/buymeacoffee.html" >> index.html
+cat "templates/google-analytics.html" >> index.html
+echo "$FOOT" >> index.html
